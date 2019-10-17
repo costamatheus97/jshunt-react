af9d2f35bcbbd686a833e791c1100a7c736e29e5
Matheus Costa <costamatheus97@live.com>
2019-10-17 20:38:54 -0300

    Aplicação Finalizada

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./styles.css";
+
+const Header = () => (
+  <header id="main-header">
+    <h1>
+      <a href="/">JSHunt</a>
+    </h1>
+  </header>
+);
+
+export default Header;
